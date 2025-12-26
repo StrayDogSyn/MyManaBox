b5fc69462ca4423adbe56600c1f31c924e4f13f5
Eric 'Hunter' Petross <eHunter@straydog-secondstory.org>
2025-12-26 05:03:53 -0500

    feat: Integrate PyQt6 for enhanced GUI capabilities and add theme system

---
+++ cardforge/qt_gui/__init__.py
@@ -0,0 +1,11 @@
+"""
+CardForge PyQt6 GUI Module
+Professional desktop interface using PyQt6
+"""
+
+from .app import CardForgeApp
+from .main_window import MainWindow
+
+__all__ = ['CardForgeApp', 'MainWindow']
+
+__version__ = '1.0.0'
